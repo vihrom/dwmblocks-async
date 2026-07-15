99c33e3baa334715dc45b07826608d390e74b847
vihrom <vitalik.khatuntsev@gmail.com>
2026-07-15 22:19:02 +0300

    custom config: async blocks for layout, volume and clock

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 #define MAX_BLOCK_OUTPUT_LENGTH 45
 
 // Control whether blocks are clickable.
-#define CLICKABLE_BLOCKS 1
+#define CLICKABLE_BLOCKS 0
 
 // Control whether a leading delimiter should be prepended to the status.
 #define LEADING_DELIMITER 0
@@ -18,16 +18,8 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-swap", 10, 0)   \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+    X("", "xkb-switch -p | tr 'a-z' 'A-Z'", 0, 11) \
+    X("", "amixer get Master | awk -F'[][]' 'END{print ($6==\"off\") ? \"MUTED\" : $2}'", 0, 8) \
+    X("", "date '+%F %R'", 60, 10)
 
 #endif  // CONFIG_H
